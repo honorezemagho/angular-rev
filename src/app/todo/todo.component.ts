89facc5855844861117df69c241f45c126d84f3e
honorezemagho <honorezemagho@gmail.com>
2020-10-17 02:19:21 +0100

    add Todo Component

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from '../models/todo';
+import {TodoService} from '../services/todo.service';
+import { FormGroup, FormControl, Validators, FormBuilder } 
+    from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit{
+
+newTodo: Todo = new Todo();
+
+    
+ constructor(private todoService: TodoService, private fb: FormBuilder) {
+
+ }
+
+Todoform = this.fb.group({
+   title: ['', Validators.required],
+  name: ['', Validators.required],
+  dateline: ['', Validators.required],
+});
+
+addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
+
+    get completeTodo() {
+    return this.todoService.completeTodo();
+  }
+
+  get inCompleteTodo(){
+    return this.todoService.inCompleteTodo();
+  }
+
+}
